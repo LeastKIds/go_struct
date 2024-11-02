1270f387d8de38fd1b108ccc39ef5ac6e865aaa3
LeastKIds <leastkids@gmail.com>
2024-11-03 00:57:54 +0900

    database dummy mapper

---
+++ internal/infrastructure/database/mapper/dummy_mapper.go
@@ -0,0 +1,28 @@
+package mapper
+
+import (
+	"github.com/LeastKIds/go_struct/internal/domain/entity"
+	"github.com/LeastKIds/go_struct/internal/infrastructure/database/model"
+)
+
+type InfrastructureDummyMapper struct{}
+
+func NewInfrastructureDummyMapper() *InfrastructureDummyMapper {
+	return &InfrastructureDummyMapper{}
+}
+
+func (m *InfrastructureDummyMapper) ToEntity(model *model.Dummy) *entity.Dummy {
+	return &entity.Dummy{
+		ID:   model.ID,
+		Name: model.Name,
+		Age:  model.Age,
+	}
+}
+
+func (m *InfrastructureDummyMapper) ToModel(entity *entity.Dummy) *model.Dummy {
+	return &model.Dummy{
+		ID:   entity.ID,
+		Name: entity.Name,
+		Age:  entity.Age,
+	}
+}
