7e70c76644494ccd9ff12ff285d66372f5477f30
LeastKIds <leastkids@gmail.com>
2024-11-02 16:33:02 +0900

    Add server start

--- internal/adapter/server/server.go
+++ internal/adapter/server/server.go
@@ -1,8 +1,20 @@
 package server
 
-func Server() {
+import "github.com/labstack/echo/v4"
 
+func Server() {
+	start()
 }
 
 func start() {
+	e := echo.New()
+
+	v1 := e.Group("/v1")
+
+	auth := v1.Group("/auth")
+	{
+		authRoutes.Routes(auth)
+	}
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
