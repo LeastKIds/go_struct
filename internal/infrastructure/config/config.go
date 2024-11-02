ac70a4d2b6d4cd6af6235885bacd7f5e7413539c
LeastKIds <leastkids@gmail.com>
2024-11-02 18:40:44 +0900

    Refactor config.go to establish connection to MySQL database

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -1,10 +1,15 @@
 package config
 
-import "github.com/LeastKIds/go_struct/internal/infrastructure/database/connection"
+import (
+	"fmt"
+
+	"github.com/LeastKIds/go_struct/internal/infrastructure/database/connection"
+)
 
 func Set() {
 	_, err := connection.NewConnection().Connect()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Connected to database")
 }
