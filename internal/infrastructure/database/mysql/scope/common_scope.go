64c1e0cd50790ad605879b0a4faa6665f17c597c
LeastKIds <leastkids@gmail.com>
2024-11-03 01:42:05 +0900

    scope

---
+++ internal/infrastructure/database/mysql/scope/common_scope.go
@@ -0,0 +1,12 @@
+package scope
+
+import (
+	"github.com/LeastKIds/go_struct/internal/domain/repository"
+	"gorm.io/gorm"
+)
+
+func ByID(id int64) repository.Scope {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Where("id = ?", id)
+	}
+}
--- internal/usecase/interactor/dummy/dummy_interactor.go
+++ internal/usecase/interactor/dummy/dummy_interactor.go
@@ -2,19 +2,23 @@ package dummy
 
 import (
 	"github.com/LeastKIds/go_struct/internal/infrastructure/database/mysql/repository"
+	"github.com/LeastKIds/go_struct/internal/infrastructure/database/mysql/scope"
 	"github.com/LeastKIds/go_struct/internal/usecase/dto"
+	"gorm.io/gorm"
 )
 
 type IInteractorDummy interface{}
 
-type InteractorDummy struct{}
-
-func NewInteractorDummy() *InteractorDummy {
-	return &InteractorDummy{}
+type InteractorDummy struct {
+	repo repository.InfrastructureRepository
 }
 
-func (i *InteractorDummy) GetDummy(repo repository.InfrastructureRepository, id int64) (*dto.Dummy, error) {
-	dummy, err := repo.InfrastructureDummyRepo.FindBy(scope.ById(id))
+func NewInteractorDummy(repo repository.InfrastructureRepository) *InteractorDummy {
+	return &InteractorDummy{repo: repo}
+}
+
+func (i *InteractorDummy) GetDummy(db *gorm.DB, id int64) (*dto.Dummy, error) {
+	dummy, err := i.repo.InfrastructureDummyRepo.FindBy(db, scope.ByID(id))
 	if err != nil {
 		return nil, err
 	}
