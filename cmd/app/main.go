ab3a67e23f80451ace46d9a64d6786a753a820dd
LeastKIds <leastkids@gmail.com>
2024-11-02 18:19:49 +0900

    Add echo framework

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,8 @@
 package main
 
+import "github.com/LeastKIds/go_struct/internal/adapter/server"
+
 func main() {
 	// Start the application
+	server.Server()
 }
--- internal/adapter/server/server.go
+++ internal/adapter/server/server.go
@@ -1,5 +1,13 @@
 package server
 
-func Server() {
+import "github.com/labstack/echo/v4"
 
+func Server() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
