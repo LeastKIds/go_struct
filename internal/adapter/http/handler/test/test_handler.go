ad2aa40b6ec050209cb3c1dcca10be48ebc5c200
LeastKIds <leastkids@gmail.com>
2024-11-02 16:43:23 +0900

    Update interactor

--- internal/adapter/http/handler/test/test_handler.go
+++ internal/adapter/http/handler/test/test_handler.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/LeastKIds/go_struct/internal/usecase/interactor/test"
 	"github.com/labstack/echo/v4"
 )
 
 func Test(c echo.Context) error {
 	fmt.Println("handler")
-	return c.String(http.StatusOK, "Test")
+	testInteractor := test.GetTestData()
+	return c.String(http.StatusOK, testInteractor)
 }
---
+++ internal/usecase/interactor/test/test_interactor.go
@@ -0,0 +1,8 @@
+package test
+
+import "fmt"
+
+func GetTestData() string {
+	fmt.Println("interactor")
+	return "Test Data"
+}
