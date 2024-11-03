e3782f14b8bdb918289d0667dd8a5e4155ea66d3
LeastKIds <leastkids@gmail.com>
2024-11-03 17:36:45 +0900

    server setting

--- internal/adapter/middleware/transaction_middleware.go
+++ internal/adapter/middleware/transaction_middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -11,8 +12,12 @@ func TransactionMiddleware(db *gorm.DB) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			if c.Request().Method == http.MethodGet {
+				fmt.Println("GET transaction")
 				c.Set("db", db)
+				return next(c)
 			}
+
+			fmt.Println("POST transaction")
 
 			tx := db.Begin()
 			if tx.Error != nil {
--- internal/adapter/server/server.go
+++ internal/adapter/server/server.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	cm "github.com/LeastKIds/go_struct/internal/adapter/middleware"
 	"github.com/LeastKIds/go_struct/internal/infrastructure/config"
 	"github.com/LeastKIds/go_struct/internal/infrastructure/database"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func Run() {
@@ -12,4 +15,10 @@ func Run() {
 		panic(err)
 	}
 
+	e := echo.New()
+	e.Use(cm.TransactionMiddleware(db))
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
